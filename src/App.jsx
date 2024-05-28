cfec6df125c46ebad6d63d117f9d36bea60b475e
florin <apetreiflorin236@gmail.com>
2024-05-28 20:25:44 +0200

    added and styled the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Home from "./HomePage";
-import NavBar from "./NavBar";
+import Home from "./HomePage.jsx";
+import NavBar from "./NavBar.jsx";
+import Sidebar from "./SideBar.jsx";
 function App() {
   return (
     <>
-        <NavBar />
-        <Home />
+      <div className="flex gap-1">
+        <div className="sidebar w-[120px]">
+          <Sidebar />
+        </div>
+        <div className="flex-1">
+          <NavBar />
+          <Home />
+        </div>
+      </div>
     </>
   );
 }
